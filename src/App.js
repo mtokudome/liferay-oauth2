4fe6b47aad665a4bf5e00f3efe075c8653a225aa
Michael Hashimoto <michael.hashimoto@liferay.com>
2023-03-01 18:26:29 -0800

    Copy the AuthorizationCodeFlow to create a ClientCredentialsFlow

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
 
 import AuthorizationCodeFlow from './components/AuthorizationCodeFlow';
+import ClientCredentialsFlow from './components/ClientCredentialsFlow';
 
 function App() {
 	return (
@@ -12,10 +13,14 @@ function App() {
 						<li>
 							<Link to='/authorization-code-flow'>Authorization Code Flow</Link>
 						</li>
+						<li>
+							<Link to='/client-credentials-flow'>Client Credentials Flow</Link>
+						</li>
 					</ul>
 				</nav>
 				<Routes>
 					<Route path='/authorization-code-flow' element={<AuthorizationCodeFlow />} />
+					<Route path='/client-credentials-flow' element={<ClientCredentialsFlow />} />
 				</Routes>
 			</div>
 		</BrowserRouter>
